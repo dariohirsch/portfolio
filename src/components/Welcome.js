2418e4de3539c8946f91bc6c1fa95ee1f2b06ed8
dariohirsch <darohirsch@gmail.com>
2022-01-05 13:14:41 +0100

    welcome section improvements

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,17 +2,19 @@ const Welcome = () => {
 	return (
 		<div id="welcome">
 			<div id="welcome-container">
-				<h4>Welcome!</h4>
+				<h4>Hi!</h4>
 				<h2>
 					I`m Dario Hirsch <br />
 					Web Developer
 				</h2>
-				<a download="Dario Hirsch CV - EN" href="../Dario Hirsch CV - English.pdf" id="downloadBtn">
-					English CV
-				</a>
-				<a download="Dario Hirsch CV - CA" href="../Dario Hirsch CV - Castellano.pdf" id="downloadBtn">
-					Spanish CV
-				</a>
+				<div id="link-cont">
+					<a dowload="Dario Hirsch CV - EN" href="../Dario Hirsch CV - English.pdf" id="downloadBtn">
+						English CV
+					</a>
+					<a download="Dario Hirsch CV - CA" href="../Dario Hirsch CV - Castellano.pdf" id="downloadBtn">
+						Spanish CV
+					</a>
+				</div>
 			</div>
 		</div>
 	)
