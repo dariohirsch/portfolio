75212ad7bb4c75600ab5a18196bc1323a4a627c8
dariohirsch <darohirsch@gmail.com>
2022-01-24 17:59:26 +0100

    fade added

--- src/components/About.js
+++ src/components/About.js
@@ -1,31 +1,35 @@
+import Fade from "react-reveal/Fade"
+
 const About = (props) => {
 	return (
-		<div id="about-me">
-			<div id="img-container">
-				<img src="/Foto Dario.jpeg" alt="profile" />
+		<Fade left cascade>
+			<div id="about-me">
+				<div id="img-container">
+					<img src="/Foto Dario.jpeg" alt="profile" />
+				</div>
+				{props.language ? (
+					<div id="about-text">
+						<p id="text">
+							{" "}
+							I'm a Jr. Fullstack Web Developer living in Barcelona. <br />
+							In my previous life, I was an accountant from the University of Buenos Aires. After some years working in that field, I decided to change my career and found my passion in the IT world. In 2021 I finished a Bootcamp in Web Development in IronHack Barcelona.
+							<br /> I'm a motivated person eager to grow in this new world. <br /> I like teamwork but I'm able to adapt to different scenarios.
+							<br /> If you have any questions, please don't hesitate to contact me!
+						</p>
+					</div>
+				) : (
+					<div id="about-text">
+						<p id="text">
+							{" "}
+							Soy un Desarrolador Fullstack Junior viviendo en Barcelona. <br />
+							En mi vida anterior fui Contador Público, recibido de la Universidad de Buenos Aires. Luego de varios años de trabajo en esa área, decidí cambiar de carrera y encontré mi pasión en el mundo de la programación. En 2021 realicé un Bootcamp de Desarrollo Web en IronHack Barcelona.
+							<br /> Soy una persona motivada y con muchas ganas de crecer en este nuevo mundo. <br /> Me gusta el trabajo en equipo pero soy capaz de adaptarme a distintos escenarios.
+							<br /> Por cualquier inquietud, no dudes en ponerte en contacto conmigo!
+						</p>
+					</div>
+				)}
 			</div>
-			{props.language ? (
-				<div id="about-text">
-					<p id="text">
-						{" "}
-						I'm a Jr. Fullstack Web Developer living in Barcelona. <br />
-						In my previous life, I was an accountant from the University of Buenos Aires. After some years working in that field, I decided to change my career and found my passion in the IT world. In 2021 I finished a Bootcamp in Web Development in IronHack Barcelona.
-						<br /> I'm a motivated person eager to grow in this new world. <br /> I like teamwork but I'm able to adapt to different scenarios.
-						<br /> If you have any questions, please don't hesitate to contact me!
-					</p>
-				</div>
-			) : (
-				<div id="about-text">
-					<p id="text">
-						{" "}
-						Soy un Desarrolador Fullstack Junior viviendo en Barcelona. <br />
-						En mi vida anterior fui Contador Público, recibido de la Universidad de Buenos Aires. Luego de varios años de trabajo en esa área, decidí cambiar de carrera y encontré mi pasión en el mundo de la programación. En 2021 realicé un Bootcamp de Desarrollo Web en IronHack Barcelona.
-						<br /> Soy una persona motivada y con muchas ganas de crecer en este nuevo mundo. <br /> Me gusta el trabajo en equipo pero soy capaz de adaptarme a distintos escenarios.
-						<br /> Por cualquier inquietud, no dudes en ponerte en contacto conmigo!
-					</p>
-				</div>
-			)}
-		</div>
+		</Fade>
 	)
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,107 +1,111 @@
+import Fade from "react-reveal/Fade"
+
 const Projects = (props) => {
 	return (
 		<div id="projects-container">
 			{props.language ? <h2>PROJECTS</h2> : <h2>PROYECTOS</h2>}
 
-			<div id="projects">
-				{props.language ? (
-					<>
-						<div className="project-card">
-							<img src="/messi in paris.jpeg" alt="" />
-							<div className="project-text">
-								<h3>Messi In Paris</h3>
+			<Fade right>
+				<div id="projects">
+					{props.language ? (
+						<>
+							<div className="project-card">
+								<img src="/messi in paris.jpeg" alt="" />
+								<div className="project-text">
+									<h3>Messi In Paris</h3>
 
-								<p> Game created using Canvas. The goal is to score as much as possible dodging cones that will appear randomly.</p>
-								<p className="tech">HTML - JS - CSS</p>
+									<p> Game created using Canvas. The goal is to score as much as possible dodging cones that will appear randomly.</p>
+									<p className="tech">HTML - JS - CSS</p>
 
-								<p>
-									<a className="gh-link" href="https://github.com/dariohirsch/First-project" target="_blank" rel="noreferrer">
-										CODE
-									</a>
-								</p>
+									<p>
+										<a className="gh-link" href="https://github.com/dariohirsch/First-project" target="_blank" rel="noreferrer">
+											CODE
+										</a>
+									</p>
 
-								<div className="workLinksContainer"></div>
+									<div className="workLinksContainer"></div>
+								</div>
 							</div>
-						</div>
-						<div className="project-card">
-							<img src="/my wish list.png" alt="" />
-							<div className="project-text">
-								<h3>My Wish List</h3>
-								<p> Web app where you can search thousands of products of your interest and add them to your wishlist. You can later share the list with your contacts. It uses the MercadoLibre API, the biggest ecommerce of LATAM to get the complete info of each product.</p>
-								<p className="tech">HTML - CSS - JS - MONGO.DB - NODE.JS</p>
-								<p>
-									<a className="gh-link" href="https://github.com/dariohirsch/second-proyect" target="_blank" rel="noreferrer">
-										CODE
-									</a>
-								</p>
-								<div className="workLinksContainer"></div>
+							<div className="project-card">
+								<img src="/my wish list.png" alt="" />
+								<div className="project-text">
+									<h3>My Wish List</h3>
+									<p> Web app where you can search thousands of products of your interest and add them to your wishlist. You can later share the list with your contacts. It uses the MercadoLibre API, the biggest ecommerce of LATAM to get the complete info of each product.</p>
+									<p className="tech">HTML - CSS - JS - MONGO.DB - NODE.JS</p>
+									<p>
+										<a className="gh-link" href="https://github.com/dariohirsch/second-proyect" target="_blank" rel="noreferrer">
+											CODE
+										</a>
+									</p>
+									<div className="workLinksContainer"></div>
+								</div>
 							</div>
-						</div>
-						<div className="project-card">
-							<img src="/betandfriends.png" alt="" />
-							<div className="project-text">
-								<h3>Bet & Friends</h3>
-								<p> Web app where you can create private or public leagues to bet in real sports playing against real people. It uses the Bet365 API to get all the competitions and their respective events and quotes. </p>
-								<p className="tech">MONGODB - EXPRESS.JS - REACT.JS - NODE.JS</p>
-								<p>
-									<a className="gh-link" href="https://github.com/dariohirsch/final-project-front" target="_blank" rel="noreferrer">
-										CODE
-									</a>
-								</p>
-								<div className="workLinksContainer"></div>
+							<div className="project-card">
+								<img src="/betandfriends.png" alt="" />
+								<div className="project-text">
+									<h3>Bet & Friends</h3>
+									<p> Web app where you can create private or public leagues to bet in real sports playing against real people. It uses the Bet365 API to get all the competitions and their respective events and quotes. </p>
+									<p className="tech">MONGODB - EXPRESS.JS - REACT.JS - NODE.JS</p>
+									<p>
+										<a className="gh-link" href="https://github.com/dariohirsch/final-project-front" target="_blank" rel="noreferrer">
+											CODE
+										</a>
+									</p>
+									<div className="workLinksContainer"></div>
+								</div>
 							</div>
-						</div>
-					</>
-				) : (
-					<>
-						<div className="project-card">
-							<img src="/messi in paris.jpeg" alt="" />
-							<div className="project-text">
-								<h3>Messi In Paris</h3>
+						</>
+					) : (
+						<>
+							<div className="project-card">
+								<img src="/messi in paris.jpeg" alt="" />
+								<div className="project-text">
+									<h3>Messi In Paris</h3>
 
-								<p> Juego creado usando Canvas. El objetivo del mismo es intentar anotar la mayor cantidad de goles posibles, esquivando los obstáculos que irán apareciendo aleatoriamente.</p>
-								<p className="tech">HTML - JS - CSS</p>
+									<p> Juego creado usando Canvas. El objetivo del mismo es intentar anotar la mayor cantidad de goles posibles, esquivando los obstáculos que irán apareciendo aleatoriamente.</p>
+									<p className="tech">HTML - JS - CSS</p>
 
-								<p>
-									<a className="gh-link" href="https://github.com/dariohirsch/First-project" target="_blank" rel="noreferrer">
-										CODE
-									</a>
-								</p>
+									<p>
+										<a className="gh-link" href="https://github.com/dariohirsch/First-project" target="_blank" rel="noreferrer">
+											CODE
+										</a>
+									</p>
 
-								<div className="workLinksContainer"></div>
+									<div className="workLinksContainer"></div>
+								</div>
 							</div>
-						</div>
-						<div className="project-card">
-							<img src="/my wish list.png" alt="" />
-							<div className="project-text">
-								<h3>My Wish List</h3>
-								<p> Aplicación web en donde podrás buscar entre miles de proctos y añadirlos a tu lista de deseos. Luego podrás compartir la lista facilmente con tus contactos. Utiliza la API de MercadoLibre, el ecommerce mas grande LATAM para obtener el producto y todos sus detalles.</p>
-								<p className="tech">HTML - CSS - JS - MONGO.DB - NODE.JS</p>
-								<p>
-									<a className="gh-link" href="https://github.com/dariohirsch/second-proyect" target="_blank" rel="noreferrer">
-										CODE
-									</a>
-								</p>
-								<div className="workLinksContainer"></div>
+							<div className="project-card">
+								<img src="/my wish list.png" alt="" />
+								<div className="project-text">
+									<h3>My Wish List</h3>
+									<p> Aplicación web en donde podrás buscar entre miles de proctos y añadirlos a tu lista de deseos. Luego podrás compartir la lista facilmente con tus contactos. Utiliza la API de MercadoLibre, el ecommerce mas grande LATAM para obtener el producto y todos sus detalles.</p>
+									<p className="tech">HTML - CSS - JS - MONGO.DB - NODE.JS</p>
+									<p>
+										<a className="gh-link" href="https://github.com/dariohirsch/second-proyect" target="_blank" rel="noreferrer">
+											CODE
+										</a>
+									</p>
+									<div className="workLinksContainer"></div>
+								</div>
 							</div>
-						</div>
-						<div className="project-card">
-							<img src="/betandfriends.png" alt="" />
-							<div className="project-text">
-								<h3>Bet & Friends</h3>
-								<p> Aplicación web en donde podrás crear ligas públicas o privadas y apostar en deportes reales contra personas reales. Utiliza la API de Bet365 para obtener la información de las distintas competiciones y sus respectivos eventos y cuotas.</p>
-								<p className="tech">MONGODB - EXPRESS.JS - REACT.JS - NODE.JS</p>
-								<p>
-									<a className="gh-link" href="https://github.com/dariohirsch/final-project-front	" target="_blank" rel="noreferrer">
-										CODE
-									</a>
-								</p>
-								<div className="workLinksContainer"></div>
+							<div className="project-card">
+								<img src="/betandfriends.png" alt="" />
+								<div className="project-text">
+									<h3>Bet & Friends</h3>
+									<p> Aplicación web en donde podrás crear ligas públicas o privadas y apostar en deportes reales contra personas reales. Utiliza la API de Bet365 para obtener la información de las distintas competiciones y sus respectivos eventos y cuotas.</p>
+									<p className="tech">MONGODB - EXPRESS.JS - REACT.JS - NODE.JS</p>
+									<p>
+										<a className="gh-link" href="https://github.com/dariohirsch/final-project-front	" target="_blank" rel="noreferrer">
+											CODE
+										</a>
+									</p>
+									<div className="workLinksContainer"></div>
+								</div>
 							</div>
-						</div>
-					</>
-				)}
-			</div>
+						</>
+					)}
+				</div>
+			</Fade>
 		</div>
 	)
 }
